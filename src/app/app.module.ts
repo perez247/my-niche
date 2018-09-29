5530b48ee49ed513b21364167f1e3bfa735cf11f
Pere <excellenceariweriokuma@gmail.com>
2018-09-29 01:50:29 +0100

    added home and about-me components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    HomeComponent,
+    AboutMeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'resume', component: ResumeComponent},
+      {path: 'about-me', component: AboutMeComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
